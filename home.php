cab25a43ede0984a14406700d57444d65c4ffd5e
H0stile <36416392+H0stile@users.noreply.github.com>
2020-07-15 15:14:53 +0200

    Update home.php

--- home.php
+++ home.php
@@ -1,7 +1,16 @@
 <?php
 
+require_once ('database.php'); 
+require_once ('navbar.php');
 
+$conn = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATA, DB_PORT);
 
+if($conn){
+
+    $query = 'SELECT * FROM movies ORDER BY views DESC LIMIT 4';
+    $result = mysqli_query($conn, $query);
+
+}
 ?>
 
 
@@ -17,5 +26,6 @@
 <body>
     <h1>Movies Share</h1>
     <h2>Welcome to the best movies database !</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod culpa ipsum rem architecto ex in corrupti rerum tempore reiciendis, repudiandae illum deleniti cumque reprehenderit. Minus ullam ipsa dolores quibusdam facilis.</p>
 </body>
 </html>
