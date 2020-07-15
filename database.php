40b457dbe4cc7a2958cc01328dce5548ebaed57c
Matt <matthieu.barbie@gmail.com>
2020-07-15 13:39:55 +0200

    Modify login and create database.php for connection

---
+++ database.php
@@ -0,0 +1,10 @@
+<?php
+
+//* Define() define a constant
+define('DB_SERVER', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATA', 'movies_share');
+define('DB_PORT', '3306');
+
+?>
