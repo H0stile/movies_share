9bea0f650c763d4c5c9b5913a271908f62b76ae7
H0stile <36416392+H0stile@users.noreply.github.com>
2020-07-15 14:55:52 +0200

    Create home.php

---
+++ home.php
@@ -0,0 +1,21 @@
+<?php
+
+
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <h1>Movies Share</h1>
+    <h2>Welcome to the best movies database !</h2>
+</body>
+</html>
