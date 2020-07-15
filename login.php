1833f7e6ce841c35fd824e8366d67de99efbc420
Matt <matthieu.barbie@gmail.com>
2020-07-15 14:50:42 +0200

    Update login.php

--- login.php
+++ login.php
@@ -4,36 +4,59 @@ session_start();
 
 require('database.php');
 
-$error = array('username'=>'', 'password'=>'');
+//* DECLARE VAR
+$errors = array('username'=>'', 'password'=>'', 'connection'=>'', 'loginFailed'=>'');
+$logOk = false;
+$username = '';
 
-
+//* CHECK "THE PUSH THE BUTTON"
 if (isset($_POST['login'])) {
     if (!empty($_POST['username'])) {
         $username = trim($_POST['username']);
         $username = strip_tags($username);
     }else{
-        $error['username'] = 'You need to put a username';
+        $errors['username'] = 'You need to put a username';
         
     }
     if (!empty($_POST['password'])) {
         $password = trim($_POST['password']);
         $password = strip_tags($password);
     }else{
-        $error['password'] = 'You need to put your password';
+        $errors['password'] = 'You need to put your password';
         
     }
+    //* IF ALL OK AND NOT SOMETHING MISSING CONNECT DB
     if (!empty($username) && !empty($password)) {
         $conn = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATA, DB_PORT);
         if ($conn) {
-            $query = "SELECT username, password FROM users";
+            $query = "SELECT username, password, user_id FROM users";
             $sendRequest = mysqli_query($conn, $query);
             $users = mysqli_fetch_all($sendRequest, MYSQLI_ASSOC);
-            var_dump($users);
+            mysqli_close($conn);
         }else{
-            $msg = 'Connection failed to the server, contact us if persist';
+            $errors = 'Connection failed to the server, contact us if persist';
         }
-    }else{
-        echo "there are error";
+        //* FOUND THE USER
+        foreach ($users as $user) {
+            if ($username === $user['username'] && password_verify($password, $user['password'])) {
+                $logOk = true;
+            break;
+            }
+        }
+        if ($logOk) {
+            $user_id = $user['user_id'];
+            if (!isset($_SESSION['user_id'])) {
+                $_SESSION['user_id'] = $user_id;
+                $username = '';
+                $password = '';
+                var_dump($user);
+                echo $_SESSION['user_id'];
+                // header("location: account.php");
+                // exit();
+            }
+        }else{
+            $errors['loginFailed'] = "Login Failed, check your informations";
+        }
     }
 }
 
@@ -52,9 +75,14 @@ if (isset($_POST['login'])) {
     ?>
     <h1>Login page</h1>
     <form method="POST">
-        <input type="text" name="username" placeholder="Username (email)">
+        <input type="text" name="username" placeholder="Username (email)" value="<?= $username?>">
         <input type="password" name="password" placeholder="Password">
         <input type="submit" name="login" value="Login">
     </form>
+    <section>
+        <?php foreach($errors as $error) :?>
+            <p><?= $error?></p>
+        <?php endforeach?>
+    </section>
 </body>
 </html>
