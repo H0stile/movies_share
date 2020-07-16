981d70c1550ad907a158c5556d5c2d3dccb14cd2
H0stile <36416392+H0stile@users.noreply.github.com>
2020-07-16 16:01:38 +0200

    navbar css

--- navbar.php
+++ navbar.php
@@ -3,15 +3,21 @@
 ?>
 
 <head>
-    <!-- <link rel="stylesheet" href="styles/style.css"> -->
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Pacifico&family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="styles/style.css">
 </head>
 
-<nav>
-    <a href="home.php">Home</a>
-    <a href="catalogue.php">Catalogue</a>
-    <a href="playlists.php">Playlists</a>
-    <a href="manageCategories.php">Manage categories</a>
-    <a href="addMovie.php">Add a movie</a>
+<nav id="navbar">
+    <div id="navBar-content">
+        <h1>MovieShare</h1>
+        <section>
+            <a href="home.php">Home</a>
+            <a href="catalogue.php">Catalogue</a>
+            <a href="playlists.php">Playlists</a>
+            <a href="manageCategories.php">Manage categories</a>
+            <a href="addMovie.php">Add a movie</a>
+       
+    
     <?php
     if (!isset($_SESSION['user_id'])) {
         echo '<a href="register.php">Register</a>';
@@ -26,5 +32,6 @@
         echo '<a href="logout.php">Logout</a>';
     }
     ?>
-    
+    </section>
+    </div>
 </nav>
