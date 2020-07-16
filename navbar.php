690a4e942c4ade5b82a86efd0901c42fb1dac75c
Matt <matthieu.barbie@gmail.com>
2020-07-16 15:21:54 +0200

    added mofidy mode

--- navbar.php
+++ navbar.php
@@ -11,7 +11,7 @@
     <a href="catalogue.php">Catalogue</a>
     <a href="playlists.php">Playlists</a>
     <a href="manageCategories.php">Manage categories</a>
-    <a href="addMovie.php">Add a movie</a>
+    <a href="modifymovies.php">Add a movie</a>
     <?php
     if (!isset($_SESSION['user_id'])) {
         echo '<a href="register.php">Register</a>';
